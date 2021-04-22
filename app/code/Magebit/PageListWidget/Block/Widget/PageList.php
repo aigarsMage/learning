2e782179cc5a23a7d60d54acdc05b20d46b877fa
Aigars <aigars.uplejs@magebit.com>
2021-04-22 09:37:30 +0300

    first commit

---
+++ app/code/Magebit/PageListWidget/Block/Widget/PageList.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aigarsuplejs
+ * Date: 21.22.4
+ * Time: 09:16
+ */
+
+namespace Magebit\Block\Widget;
+
+
+class PageList
+{
+
+}
---
+++ app/code/Magebit/PageListWidget/registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aigarsuplejs
+ * Date: 21.22.4
+ * Time: 09:19
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Magebit_PageListWidget',
+    __DIR__
+);
