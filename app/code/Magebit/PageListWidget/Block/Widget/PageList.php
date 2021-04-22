84b9d48f7296b1e5a746117f7170b1de0fccc2b2
Aigars <aigars.uplejs@magebit.com>
2021-04-22 15:38:56 +0300

    admin front end done

--- app/code/Magebit/PageListWidget/Block/Widget/PageList.php
+++ app/code/Magebit/PageListWidget/Block/Widget/PageList.php
@@ -6,10 +6,12 @@
  * Time: 09:16
  */
 
-namespace Magebit\Block\Widget;
+namespace Magebit\PageListWidget\Block\Widget;
 
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
 
-class PageList
+class PageList extends Template implements BlockInterface
 {
-
+    protected $_template = "page-list.phtml";
 }
