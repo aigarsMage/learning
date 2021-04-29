259c7a5c707e3e6f115da61e442560e206343fad
Aigars <aigars.uplejs@magebit.com>
2021-04-29 12:46:40 +0300

    small changes for styles

--- app/design/frontend/Magebit/learning/Magento_Catalog/requirejs-config.js
+++ app/design/frontend/Magebit/learning/Magento_Catalog/requirejs-config.js
@@ -3,5 +3,13 @@ var config = {
         '*': {
             'qty-counter': 'Magento_Catalog/js/qty-counter'
         }
+    },
+    paths: {
+        'show-hide-addtocart': 'Magento_Catalog/js/show-hide-addtocart'
+    },
+    shim: {
+        'show-hide-addtocart': {
+            deps: ['jquery']
+        }
     }
 };
